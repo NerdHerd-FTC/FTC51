9aa348e38224398250fc59df35c1c1f642851033
Tonnieboy300 <tonnieboy300@icloud.com>
2023-09-15 16:13:07 -0700

    Revert "Update mecanumDrive.java"
    This reverts commit c3688b1bcbcf8d686d419fcb468ce81cfa8b57e2.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
@@ -69,12 +69,12 @@ public class mecanumDrive extends LinearOpMode {
             //at least one power is <-1 or >1
             double denominator = Math.max(Math.abs(rotationY) + Math.abs(rotationX) + Math.abs(rStickX), 1);
 
-            double flPower = (rotationY + rotationX + rStickX) / denominator; // cool calculation
+            double flPower = (rotationY + rotationX + rStickX) / denominator;
             double frPower = (rotationY - rotationX - rStickX) / denominator;
             double blPower = (rotationY - rotationX + rStickX) / denominator;
             double brPower = (rotationY + rotationX - rStickX) / denominator;
 
-            flMotor.setPower(flPower); // move the motor
+            flMotor.setPower(flPower);
             frMotor.setPower(frPower);
             blMotor.setPower(blPower);
             brMotor.setPower(brPower);
