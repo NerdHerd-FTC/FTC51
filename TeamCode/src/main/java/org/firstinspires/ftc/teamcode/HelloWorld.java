5277a5dcf8f302229b4b4bf64931f1eb14b01025
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-09-15 16:47:48 -0700

    Create HelloWorld.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name= "Hello World", group="Robot")
+public class HelloWorld extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        telemetry.addLine("Hello World!");
+        telemetry.update();
+        waitForStart();
+        int number = 0;
+        while (opModeIsActive()) {
+            number++;
+            telemetry.addData("the secret number", 10 - number);
+            telemetry.addData("the secret number pt 2", number + 13);
+        }
+    }
+}
