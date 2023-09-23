0356887eb9ba0a2a3d2ed16b0c85d172bf9347c7
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-09-23 14:48:40 -0700

    comment out unused import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionExample.java
@@ -7,7 +7,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+//import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 import java.util.List;
 
