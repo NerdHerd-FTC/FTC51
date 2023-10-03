97fab8289fdb8750ba5c29e2d10e03b44d174b48
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-10-02 17:22:21 -0700

    Add comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoExample.java
@@ -7,43 +7,49 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name="Auto Example")
 public class autoExample extends LinearOpMode {
+    // Define motor variables
     private final DcMotor flMotor = hardwareMap.dcMotor.get("motorFL");
     private final DcMotor frMotor = hardwareMap.dcMotor.get("motorFR");
     private final DcMotor blMotor = hardwareMap.dcMotor.get("motorBL");
     private final DcMotor brMotor = hardwareMap.dcMotor.get("motorBR");
-
     private final DcMotor slideMotor = hardwareMap.dcMotor.get("motorSlide");
+    // Immutable
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        //reverse right side motors. reverse left side if goes backwards
+        // Set motor directions
         frMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         brMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         flMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         blMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         slideMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        // Reset the encoder
         flMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         brMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        // Sets motors into go to position mode
         flMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         brMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // Switches from setting power to moving to a position
 
         waitForStart();
 
-        //distance is in mm
-        //create thread for async running
+        // Move robot forward 150 meters
         Thread runDrive = new Thread(() -> driveFunc(150));
         runDrive.start();
+        // Using a thread to move and continue code without waiting
 
-        telemetry.addLine("wow it run it good");
+        telemetry.addLine("Drive has started");
         telemetry.update();
     }
 
     public void driveFunc(double distance){
+        // Moves the robot the distance forward
+        // Distance is in millimeters
 
         //537.7 Pulses per Rotation
         //PROBABLY 2148 counts per rotation
