ac2f6a461cd4ebf342b0a203a41d272cf37759ce
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-10-16 16:42:10 -0700

    Add strafe controls for auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoExample.java
@@ -45,18 +45,18 @@ public class autoExample extends LinearOpMode {
 
         telemetry.addLine("Drive has started"); // Test to see if multithreading works
         telemetry.update();
-        // If the telemetry does not display until driving has finished, it dont work.
+        // If the telemetry does not display until driving has finished, it don't work.
     }
+
+    //537.7 Pulses per Rotation
+    //PROBABLY 2148 counts per rotation
+    //Wheels are 96mm diameter
+    final double countsPerMM = 2148 / (96 * Math.PI);
+    // Used to calculate the amount to move each motor
 
     public void driveFunc(double distance){
         // Moves the robot the distance forward
         // Distance is in millimeters
-
-        //537.7 Pulses per Rotation
-        //PROBABLY 2148 counts per rotation
-        //Wheels are 96mm diameter
-        double countsPerMM = 2148 / (96 * Math.PI);
-        // Used to calculate the amount to move each motor
 
         flMotor.setTargetPosition(flMotor.getCurrentPosition() + (int) (distance * countsPerMM)); // Tell motors to move
         brMotor.setTargetPosition(brMotor.getCurrentPosition() + (int) (distance * countsPerMM));
@@ -78,4 +78,26 @@ public class autoExample extends LinearOpMode {
         telemetry.addLine("Drive has finished");
         telemetry.update();
     }
+
+    public void strafe(double distance, double direction){
+        flMotor.setTargetPosition(flMotor.getCurrentPosition() + (int) ((distance * countsPerMM) * direction));
+        brMotor.setTargetPosition(brMotor.getCurrentPosition() + (int) ((distance * countsPerMM) * direction));
+
+        // We only have a few encoder ports, so manually move the two other motors
+        frMotor.setPower(-direction);
+        blMotor.setPower(-direction);
+
+        // Wait for motors to stop moving
+        while (flMotor.isBusy() || brMotor.isBusy()) {;} // busy loop; we need to fix later
+        // maybe never if it works fine
+
+        frMotor.setPower(0); // Stop motors
+        blMotor.setPower(0);
+
+        // REMOVE AFTER TESTING THREADS
+        telemetry.addLine("Strafe has finished");
+        telemetry.update();
+
+    }
+
 }
