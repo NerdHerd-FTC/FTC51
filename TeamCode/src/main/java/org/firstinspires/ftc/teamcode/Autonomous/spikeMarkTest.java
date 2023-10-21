b2804ef45c373eb94e712132a5b00032db7cc3e2
Tonnieboy300 <tonnieboy300@icloud.com>
2023-10-20 18:38:00 -0700

    add parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/spikeMarkTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/spikeMarkTest.java
@@ -3,12 +3,46 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="spikemarktest", group = "Tests")
 public class spikeMarkTest extends autoExample {
     @Override
     public void runOpMode() throws InterruptedException{
+
+        // Set motor directions
+        frMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        brMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        flMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        blMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        slideMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // Reset the encoder
+        flMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // fix drivetrain motor directions
+        frMotor.setDirection(DcMotorSimple.Direction.REVERSE); // IDK MAN, WE NEED TO TEST
+        flMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        brMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        blMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        flMotor.setTargetPosition(0);
+        brMotor.setTargetPosition(0);
+        slideMotor.setTargetPosition(0);
+
+        flMotor.setPower(1);
+        brMotor.setPower(1);
+        slideMotor.setPower(1);
+
+        // Sets motors into go to position mode
+        flMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // Switches from setting power to moving to a position
+        
         waitForStart();
 
         while(opModeIsActive()){
