af2d8ca743601fda80e0109bdd49bcd5c76f8b30
Tonnieboy300 <tonnieboy300@icloud.com>
2024-02-17 19:06:19 -0800

    ensure intake servo buttons input only once
    Adds variables "intakeLButton", "intakeRButton", and "intakeMButton" to track if that button has been pushed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrains/armControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrains/armControls.java
@@ -28,8 +28,11 @@ public class armControls extends LinearOpMode {
     double intakeServoMDown = 0.5;
 
     boolean intakeLUp = false;
+    boolean intakeLButton = false;
     boolean intakeMUp = false;
+    boolean intakeMButton = false;
     boolean intakeRUp = false;
+    boolean intakeRButton = false;
 
     public String armControls(DcMotor slideMotorR, DcMotor slideMotorL, Servo armTopServoR, Servo armTopServoL, DcMotor intakeMotor, Servo droneServo, Servo intakeServoL, Servo intakeServoM, Servo intakeServoR, Gamepad gamepad1, Gamepad gamepad2) {
         String currentTelemetry = "";
@@ -42,24 +45,24 @@ public class armControls extends LinearOpMode {
             if (!directionButtonPressed) {
                 direction = -direction;
                 intakeMotor.setPower(intakeMotor.getPower() * -1);
-                directionButtonPressed=true;
+                directionButtonPressed = true;
             }
         } else {
-            directionButtonPressed=false;
+            directionButtonPressed = false;
         }
         if (gamepad1.a) { // Toggle intake motor on/off
             if (!intakeButtonPressed) {
-                intakeMotor.setPower((1 - Math.abs(intakeMotor.getPower()))*direction);
+                intakeMotor.setPower((1 - Math.abs(intakeMotor.getPower())) * direction);
                 intakeButtonPressed = true;
             }
         } else {
             intakeButtonPressed = false;
         }
-        currentTelemetry+= "Intake power : "+ Math.abs(intakeMotor.getPower());
-        currentTelemetry+= "\nIntake direction : " + ((direction==-1) ? "Forward" : "Backward");
+        currentTelemetry += "Intake power : " + Math.abs(intakeMotor.getPower());
+        currentTelemetry += "\nIntake direction : " + ((direction == -1) ? "Forward" : "Backward");
 
         //Servos are 0-1 with a range of 300 degrees
-        if (gamepad2.right_bumper){
+        if (gamepad2.right_bumper) {
             armTopServoR.setPosition(servoReleasePosition);
             armTopServoL.setPosition(servoReleasePosition);
         } else if (gamepad2.left_bumper) {
@@ -67,33 +70,49 @@ public class armControls extends LinearOpMode {
             armTopServoL.setPosition(servoLoadPosition);
         }
 
-        currentTelemetry+= "\nServo Position: "+ armTopServoR.getPosition();
+        currentTelemetry += "\nServo Position: " + armTopServoR.getPosition();
 
         if (gamepad1.left_bumper){
-            intakeLUp = !intakeLUp;
-            if (intakeLUp) {
-                intakeServoL.setPosition(intakeServoUp);
-            } else {
-                intakeServoL.setPosition(intakeServoDown);
+            if (!intakeLButton){
+                intakeLUp = !intakeLUp;
+                if (intakeLUp) {
+                    intakeServoL.setPosition(intakeServoUp);
+                } else {
+                    intakeServoL.setPosition(intakeServoDown);
+                }
             }
+            intakeLButton = true;
+        } else {
+            intakeLButton = false;
         }
 
 //        if (gamepad1.x){
-//            intakeMUp = !intakeMUp;
-//            if (intakeMUp) {
-//                intakeServoM.setPosition(intakeServoMUp);
-//            } else {
-//                intakeServoM.setPosition(intakeServoMDown);
+//            if (!intakeMButton){
+//                intakeMUp = !intakeMUp;
+//                if (intakeMUp) {
+//                    intakeServoM.setPosition(intakeServoMUp);
+//                } else {
+//                    intakeServoM.setPosition(intakeServoMDown);
+//                }
 //            }
+//            intakeMButton = true;
+//        } else {
+//            intakeMButton = false;
 //        }
 
+
         if (gamepad1.right_bumper){
-            intakeRUp = !intakeRUp;
-            if (intakeRUp) {
-                intakeServoR.setPosition(intakeServoUp);
-            } else {
-                intakeServoR.setPosition(intakeServoDown);
+            if (!intakeRButton){
+                intakeRUp = !intakeRUp;
+                if (intakeRUp) {
+                    intakeServoR.setPosition(intakeServoUp);
+                } else {
+                    intakeServoR.setPosition(intakeServoDown);
+                }
             }
+            intakeRButton = true;
+        } else {
+            intakeRButton = false;
         }
 
         //moves the drone servo to the launch position
